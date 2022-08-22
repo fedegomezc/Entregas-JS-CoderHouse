60bd525042ccfda4bc7424d21e7b234bd62068f4
Federico Gomez Conde <fedeic8@gmail.com>
2022-08-22 10:07:54 -0300

    Se agrega casos de números negativos

--- Algoritmo ciclo/JS/ciclo.js
+++ Algoritmo ciclo/JS/ciclo.js
@@ -4,10 +4,17 @@ de dígitos que tiene el número
 */
 
 let numero = parseInt(prompt("Ingrese un número entero:"))
+// Se inicializa un contador en 0
 let cantidad = 0
 
+// Si se ingresa un numero negativo se convierte a positivo para operarlo
+if (numero < 0){
+    numero = numero*(-1)
+}
+// mientras el numero tenga digitos se contabiliza 1 y se quita un dígito
 while (numero > 0){
     cantidad += 1
     numero = parseInt(numero/10)
 }
+
 console.log("El número ingresado tiene", cantidad, "dígitos")
